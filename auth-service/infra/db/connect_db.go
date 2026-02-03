9e11b0ce6064df6edc908f45907b9e59bd33ba4e
copyniinja <killfyz0.o@gmail.com>
2026-02-03 19:09:36 +0600

    feat: dockerize auth service and add auth DB retry logic

--- auth-service/infra/db/connect_db.go
+++ auth-service/infra/db/connect_db.go
@@ -1,25 +1,62 @@
 package db
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
+	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-const DATABASE_URL = `postgres://faiyaz:faiyaz@localhost:5432/auth_db`
-
 func NewConnection() *sql.DB {
-	conn, err := sql.Open("pgx", DATABASE_URL)
+
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		user, password, host, port, dbName,
+	)
+
+	var db *sql.DB
+	var err error
+
+	maxRetries := 10
+
+	for i := 1; i <= maxRetries; i++ {
+
+		db, err = sql.Open("pgx", dsn)
+		if err != nil {
+			log.Printf("DB open error: %v", err)
+			time.Sleep(2 * time.Second)
+			continue
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		err = db.PingContext(ctx)
+		cancel()
+
+		if err == nil {
+			log.Println("Connected to Auth database.")
+			break
+		}
+
+		log.Printf("DB not ready (attempt %d/%d): %v", i, maxRetries, err)
+		time.Sleep(2 * time.Second)
+	}
 
 	if err != nil {
-		log.Fatalf("Unable to connect to database:%v", err)
-
+		log.Fatal("Could not connect to database after retries")
 	}
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(10)
+	db.SetConnMaxLifetime(5 * time.Minute)
 
-	if err := conn.Ping(); err != nil {
-		log.Fatalf("DB not reachable: %v", err)
-	}
-
-	return conn
+	return db
 }
